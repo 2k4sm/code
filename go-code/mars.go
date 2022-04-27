4810e6f2f828a76bcafa110cfa6c48a770ae66c9
2k4sm <shrinibasmahanta2004@gmail.com>
2022-04-27 22:01:30 +0530

    mars weight calculator.

---
+++ go-code/mars.go
@@ -0,0 +1,16 @@
+// program to calculate weight lost on mars....//
+//mars year has 687 days but earth has 365 days.//
+//weight on mars is 38% of what on earth...//
+
+package main
+
+import "fmt"
+
+func main(){
+
+  fmt.Println("Weight on mars is:",60*0.38)
+  fmt.Println("My age on mars will be:",19*365/687)
+
+
+
+}
