98397d7ba5529ef11af8420fbba3f751b33da2ea
2k4sm <shrinibasmahanta2004@gmail.com>
2022-05-02 23:51:42 +0530

    arrey creation

---
+++ go-code/arr.go
@@ -0,0 +1,15 @@
+package main
+import (
+   "fmt"
+)
+func main(){
+   fmt.Printf("Enter size of your array: ")
+   var size int
+   fmt.Scanln(&size)
+   var arr = make([]int, size)
+   for i:=0; i<size; i++ {
+      fmt.Printf("Enter %dth element: ", i)
+      fmt.Scanf("%d", &arr[i])
+   }
+   fmt.Println("Your array is: ", arr)
+}
