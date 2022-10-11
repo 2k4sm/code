39c2ce61fcf3127943942a81f84d651ab4f5214a
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2022-10-11 20:06:16 +0530

    removes ctof.go adds conv funcs into tempconv

--- go-code/tempconv/tempconv.go
+++ go-code/tempconv/tempconv.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"fmt"
-
-	T "./temperature"
 )
 
 func main() {
-	fmt.Println(Ktoc(233))
-	fmt.Println(T.Ctof(Ktoc(233)))
-	fmt.Println(T.Ktof(0))
+	fmt.Println(Ktoc(273))
+	fmt.Println(Ctof(Ktoc(273)))
+	fmt.Println(Ktof(0))
 }
 func Ktoc(temp int) int {
 	const kelv0 int = 273
 
 	return temp - kelv0
 
+}
+func Ctof(temp int) int {
+	f := (temp * 9.0 / 5.0) + 32.0
+	return f
+}
+func Ktof(temp int) int {
+	c := temp - 273
+
+	f := (c * 9.0 / 5.0) + 32.0
+
+	return f
 }
