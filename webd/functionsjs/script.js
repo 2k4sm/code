e6a34dce5a6605f7a19d0894a13ecdbd1a9b597e
sm2k4 <shrinibasmahanta2004@gmail.com>
2023-02-20 18:43:26 +0530

    .

--- webd/functionsjs/script.js
+++ webd/functionsjs/script.js
@@ -13,32 +13,3 @@ let lastLetter = word => h1.innerHTML = word[word.length-1]
 
 
 
-'''
-we defined the constants 'EXPECTED_BAKE_TIME' and 'PREPARATION_TIME'
-'''
-
-EXPECTED_BAKE_TIME = 40
-PREPARATION_TIME = 2
-
-def bake_time_remaining(elapsed_bake_time):
-    '''
-    This function takes the elapsed_bake_time as argument and return bake time remaining.
-    '''
-    return EXPECTED_BAKE_TIME - elapsed_bake_time
-
-
-
-def preparation_time_in_minutes(layers):
-    '''
-    This function takes the layers as argument and return preparation time.
-    '''
-    return PREPARATION_TIME * layers
-
-
-
-def elapsed_time_in_minutes(layers,elapsed_bake_time):
-    '''
-    This function takes layers and  elapsed_bake_time as argument and return elapsed time.
-    '''
-    return preparation_time_in_minutes(layers)+elapsed_bake_time
-
