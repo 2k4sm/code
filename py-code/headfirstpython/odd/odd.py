14caf3a3e773a4af8848705730eb2ef505ae967b
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2023-05-09 20:36:39 +0530

    learnpython

---
+++ py-code/headfirstpython/odd/odd.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+import sleep
+import randint
+
+odds = [1,3,5,7,9,11,13,15,17,19,
+	21,23,25,27,29,31,33,35,
+	37,39,41,43,45,47,49,51,
+	53,55,57,59]
+
+for i in range(0,5):
+	right_this_minute = datetime.today().minute
+
+	if right_this_minute in odds:
+		print("This minute seems a little odd.")
+		time.sleep(random.randint(1,60))
+	else:
+		print("Not an odd minute.")
+		time.sleep(random.randint(1,60))
